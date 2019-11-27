6be400fa8c1562f966b676d72309b80c61488a43
Jonathan Norman <jnorman2123@gmail.com>
2019-11-27 15:08:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,15 +6,5 @@ class App < Sinatra::Base
     "Hello World"
   end
 
-  get '/name' do
-    "My name is Jonathan."
-  end
 
-  get '/hometown' do
-    "My hometown is Atlanta, Ga."
-  end
-
-  get '/favorite-song' do
-    "My favorite song is Passenger Seat by Death Cab for Cutie."
-  end
 end
